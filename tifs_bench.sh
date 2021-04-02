f0c120fce368148991a9914238205e4d21c77247
Hirozawa Kazuma <kazumasita@easter.kuee.kyoto-u.ac.jp>
2021-04-02 18:52:10 +0900

    add tifs benchmark script

---
+++ tifs_bench.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+input_n=(4 8 16 32 64)
+input_dims=()
+kernel_dim=4
+err=tifs_result/err.log
+
+for n in ${input_n[@]}
+do
+    echo $n
+    nn=$(($n ** 2))
+    input_dims=("${input_dims[@]}" $nn)
+done
+
+# assume executable file is build/bin/direct_conv
+function direct_conv(){
+    mkdir -p tifs_result/
+    for input_dim in ${input_dims[@]}
+    do
+        echo "Direct Convolution: (input, kernel) = ($input_dim, $kernel_dim)"
+        output_path="tifs_result/direct_conv$input_dim.txt"
+        env build/bin/direct_conv $input_dim $kernel_dim 2>$err > $output_path
+    done
+}
+
+direct_conv
