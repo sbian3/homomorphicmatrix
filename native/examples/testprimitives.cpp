5b6e40ff590670f2b545ad8db6570da08170d4c3
Hirozawa Kazuma <kazumasita@easter.kuee.kyoto-u.ac.jp>
2021-04-02 21:47:18 +0900

    fix bug of calc_product_diagonal

--- native/examples/testprimitives.cpp
+++ native/examples/testprimitives.cpp
@@ -5,23 +5,38 @@
 
 // assume kernel_L is reversed( indexes is also )
 vector<uint64_t> calc_product_diagonal(int64_t offset, uint64_t colsize_R, uint64_t rowsize_R, vector<uint64_t> kernel_L, vector<uint64_t> kernel_L_indexes, vector<uint64_t> list_R, Modulus & modulus){
-    // TODO indexの変更と(ソート)
+    // RはL以上を想定
     assert(kernel_L_indexes.size() <= list_R.size());
 
     // 要素積を計算
     // kernelの非ゼロ要素の場所を覚えることで計算時間を短縮
-    uint64_t wise_prod_len = kernel_L.size() <= list_R.size()? kernel_L.size(): list_R.size();
+    //uint64_t wise_prod_len = kernel_L.size() <= list_R.size()? kernel_L.size(): list_R.size();
+    uint64_t wise_prod_len;
+    if(offset >= 0){
+        if(offset + kernel_L.size() > list_R.size()){
+            wise_prod_len = list_R.size() - offset;
+        }else{
+            wise_prod_len = kernel_L.size(); 
+        }
+    }else{
+        wise_prod_len = kernel_L.size() + offset;
+    }
     vector<uint64_t> wise_prod(wise_prod_len);
     for(uint64_t i = 0;i < kernel_L_indexes.size();i++){
         uint64_t prod;
         if(offset >= 0){
-            if(kernel_L_indexes[i]+offset >= list_R.size() || kernel_L_indexes[i] >= kernel_L.size()) continue;
+            if(kernel_L_indexes[i]+offset >= list_R.size() || kernel_L_indexes[i] >= kernel_L.size()) break;
             prod = util::multiply_uint_mod(kernel_L[kernel_L_indexes[i]], list_R[kernel_L_indexes[i]+offset], modulus);
         }else{
-            if(kernel_L_indexes[i]-offset >= kernel_L.size() || kernel_L_indexes[i] >= list_R.size()) continue;
-            prod = util::multiply_uint_mod(kernel_L[kernel_L_indexes[i]-offset], list_R[kernel_L_indexes[i]], modulus);
+            if(kernel_L_indexes[i] <= -offset-1) continue;
+            if(kernel_L_indexes[i]-offset >= kernel_L.size() || kernel_L_indexes[i] >= list_R.size()) break;
+            prod = util::multiply_uint_mod(kernel_L[kernel_L_indexes[i]], list_R[kernel_L_indexes[i]+offset], modulus);
         }
-        wise_prod[kernel_L_indexes[i]] = prod;
+        if(offset < 0){
+            wise_prod[kernel_L_indexes[i]+offset] = prod;
+        }else{
+            wise_prod[kernel_L_indexes[i]] = prod;
+        }
     }
     vector<uint64_t> diagonal;
     uint64_t partial_sum = 0;
@@ -51,7 +66,7 @@ void test_prod_diagonal(){
     Modulus modulus(11);
     vector<uint64_t> kernel_diagonallist(colsize_K + rowsize_K - 1);
     vector<uint64_t> indexes = util::create_diagonal_list(kernel, colsize_K, rowsize_K, modulus, kernel_diagonallist);
-    vector<uint64_t> diagonal_0 = calc_product_diagonal(1, colsize_R, rowsize_R, kernel_diagonallist, indexes, list_R, modulus);
+    vector<uint64_t> diagonal_0 = calc_product_diagonal(-3, colsize_R, rowsize_R, kernel_diagonallist, indexes, list_R, modulus);
     for(auto i = 0;i < diagonal_0.size();i++){
         cout << diagonal_0[i] << " ";
     }
