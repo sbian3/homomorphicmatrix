7d97219c20eae51e83325ab8d215f5ab62813b9d
Hirozawa Kazuma <kazumasita@easter.kuee.kyoto-u.ac.jp>
2021-05-18 14:39:35 +0900

    remove seal library

--- native/examples/simd.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void test_batch_encoder();
-void test_batch_convolution();
-void test_batch_matrix();
--- native/src/seal/util/define_tifs.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define TIFS_DEBUG 0
-#define TIFS_DEBUG_TIME 0
