7fbba2e113834163c41c16c2bb71f04d9846399b
kazbno <kazumasita@gmail.com>
2022-01-27 13:53:33 +0900

    add decryption plotting

--- plotting/tifs_plot.py
+++ plotting/tifs_plot.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from enum import Enum
 
-poly_degrees = [ 1024, 2048, 4096, 8192, 16384]
+poly_degrees = [ 1024, 2048, 4096, 8192]
 #poly_degrees = [ 1024, 2048, 4096, 8192, 16384, 32768]
-kernel_d=25
+kernel_d=4
 kernel_dims = [2, 4, 8, 16, 32, 64, 128]
 #input_dims = [16, 64, 256, 1024]
 input_dims = [16, 32, 64, 128, 256, 512, 1024]
@@ -42,75 +42,113 @@ def read_latency(filepath):
         line = line.rstrip()
         line = line.split(" ")
         if(line[0] == lt_prefix):
-            # current unit is [ms]
+            # current time unit is [ms]
             if(line[2] == TimeUnit.ms.value):
                 time_lt = float(line[1])
+                timeunit = TimeUnit.ms
             elif(line[2] == TimeUnit.us.value):
-                # if [us] in result txt, devide by 1000
+                # if time unit is [us] in result txt, devide by 1000
                 time_lt = float(line[1])/1000
+                timeunit = TimeUnit.us
         if(line[0] == dec_prefix):
-            time_dec = float(line[1])
-    return time_lt, time_dec
+            if(line[2] == TimeUnit.ms.value):
+                time_dec = float(line[1])
+                timeunit = TimeUnit.ms
+            elif(line[2] == TimeUnit.us.value):
+                time_dec = float(line[1])/1000
+                timeunit = TimeUnit.us
+    return time_lt, time_dec, timeunit
 
 def join_path(result_dirname, algorithm_name, benchtype, param_bench):
     slash = "/"
     filepath = "../" + result_dirname + slash + algorithm_name + slash + benchtype.value + slash + str(param_bench) + ".txt"
     return filepath
 
+class HltData():
+    def __init__(self, lt_time, dec_time, timeunit):
+        self.lt_time = lt_time
+        self.dec_time = dec_time
+        self.timeunit = timeunit
+
 # read bench data from ../hlt_result
-def read_bench(prefix, benchtype, kernel_dim=0):
-    bench_lt = []
+def read_bench(algorithm_name, benchtype, kernel_dim=0):
+    lt_times = []
+    dec_times = []
+    result_dirname = "../hlt_result"
     if(benchtype == BenchType.multi_poly):
         for poly_degree in poly_degrees:
-            filepath = "../tifs_result/" + prefix +   "/"+ benchtype.value+ "/" + "k" + str(kernel_dim) + "/" +  prefix + str(poly_degree) + ".txt"
-            time_lt, time_dec = read_latency(filepath)
-            bench_lt.append(time_lt)
+            filepath = result_dirname + "/" + algorithm_name +   "/"+ benchtype.value+ "/" + "k" + str(kernel_dim) + "/" +  algorithm_name + str(poly_degree) + ".txt"
+            time_lt, time_dec, timeunit = read_latency(filepath)
+            lt_times.append(time_lt)
+            dec_times.append(time_dec)
     elif(benchtype == BenchType.multi_kernel):
         for kernel_dim in kernel_dims:
-            filepath = "../tifs_result/" + prefix +   "/"+ benchtype.value + "/" + prefix + str(kernel_dim) + ".txt"
-            time_lt, time_dec = read_latency(filepath)
-            bench_lt.append(time_lt)
+            filepath = result_dirname + "/" + algorithm_name +   "/"+ benchtype.value + "/" + algorithm_name + str(kernel_dim) + ".txt"
+            time_lt, time_dec, timeunit = read_latency(filepath)
+            lt_times.append(time_lt)
+            dec_times.append(time_dec)
     elif(benchtype == BenchType.multi_input):
         for input_dim in input_dims:
-            filepath = "../tifs_result/" + prefix +   "/"+ benchtype.value + "/" + prefix + str(input_dim) + ".txt"
-            time_lt, time_dec = read_latency(filepath)
-            bench_lt.append(time_lt)
-    return bench_lt
+            filepath = result_dirname + "/" + algorithm_name +   "/"+ benchtype.value + "/" + algorithm_name + str(input_dim) + ".txt"
+            time_lt, time_dec, timeunit = read_latency(filepath)
+            lt_times.append(time_lt)
+            dec_times.append(time_dec)
+    hltdata = HltData(lt_times, dec_times, timeunit)
+    return hltdata
 
 def read_data(benchtype, kernel_dim=0):
-    time_direct = read_bench("direct_conv", benchtype, kernel_dim)
-    time_packed = read_bench("packed_conv", benchtype, kernel_dim)
+    data_direct = read_bench("direct_conv", benchtype, kernel_dim)
+    data_packed = read_bench("packed_conv", benchtype, kernel_dim)
+    data_pcdec = read_bench("pc_toeplitz", benchtype, kernel_dim)
     # time_general is too long: put result directly
     time_general=[52570, 462696, 3234320]
-    return time_direct,time_packed, time_general
+    return data_direct, data_packed, time_general, data_pcdec
 
-def plot(x, x_label, time_direct, time_packed, time_general, benchtype, kernel_dim=0):
+def plot_lt(x, x_label, data_direct, data_packed, time_general, data_pcdec, benchtype, kernel_dim=0):
     fig, ax = plt.subplots()
-    ax.set_xscale('log', basex=2)
-    ax.set_yscale('log', basey=10)
+    ax.set_xscale('log', base=2)
+    ax.set_yscale('log', base=10)
     # direct conv
-    #ax.plot(x, time_direct, label="direct", color="red")
+    #ax.plot(x, data_direct.lt_time, label="direct", color="red")
     # packed conv
-    ax.plot(x, time_packed, label="packed conv", color="orange")
+    ax.plot(x, data_packed.lt_time, label="optimized", color="orange")
     # general_LT
-    #ax.plot(x[:3], time_general, label="general", color="blue")
+    ax.plot(x[:3], time_general, label="general_linear", color="blue")
     #ax.plot(x, ref_nsq, label="nsquare(ref)", color="green")
     #ax.plot(x, ref_n15, label="n1.5(ref)", color="purple")
     #ax.plot(x, ref_n1, label="n1(ref)", color="magenta")
-    ax.legend()
+    ax.legend(loc="upper left")
     ax.set_xlabel(x_label)
     ax.set_xticks(x)
     ax.set_ylabel("latency [ms]")
-    fig.savefig(benchtype.value + str(kernel_dim) + ".pdf", format="pdf")
-    #fig.savefig(benchtype.value + str(kernel_dim) + ".png")
+    fig_dirname = "./fig"
+    fig.savefig(fig_dirname + "/" + "lt" + benchtype.value + str(kernel_dim) + ".pdf", format="pdf", bbox_inches="tight", pad_inches=0.1)
+
+def plot_dec(x, x_label, data_direct, data_packed, time_general, data_pcdec, benchtype, kernel_dim=0):
+    fig, ax = plt.subplots()
+    ax.set_xscale('log', base=2)
+    ax.set_yscale('log', base=10)
+    # direct conv
+    #ax.plot(x, data_direct.lt_time, label="direct", color="red")
+    # packed conv
+    ax.plot(x, data_packed.dec_time, label="general_linear", color="blue")
+    # toeplitz dec
+    ax.plot(x, data_pcdec.dec_time, label="optimized", color="orange")
+    ax.legend(loc="upper left")
+    ax.set_xlabel(x_label)
+    ax.set_xticks(x)
+    ax.set_ylabel("latency [ms]")
+    fig_dirname = "./fig"
+    fig.savefig(fig_dirname + "/" + "dec" + benchtype.value + str(kernel_dim) + ".pdf", format="pdf", bbox_inches="tight", pad_inches=0.1)
 
 def plot_tifs():
-    #time_direct,time_packed, time_general = read_data(BenchType.multi_poly, kernel_d)
-    #plot(poly_degrees, "polynomial degree", time_direct, time_packed, time_general, BenchType.multi_poly, kernel_d)
+    data_direct ,data_packed, time_general, data_pcdec = read_data(BenchType.multi_poly, kernel_d)
+    plot_lt(poly_degrees, "polynomial degree", data_direct, data_packed, time_general, data_pcdec, BenchType.multi_poly, kernel_d)
+    plot_dec(poly_degrees, "polynomial degree", data_direct, data_packed, time_general, data_pcdec, BenchType.multi_poly, kernel_d)
     #time_direct,time_packed, time_general = read_data(BenchType.multi_kernel)
     #plot(kernel_dims, "kernel dimensions", time_direct, time_packed, time_general, BenchType.multi_kernel)
-    time_direct,time_packed, time_general = read_data(BenchType.multi_input)
-    plot(input_dims, "input dimensions", time_direct, time_packed, time_general, BenchType.multi_input)
+    #time_direct,time_packed, time_general = read_data(BenchType.multi_input)
+    #plot(input_dims, "input dimensions", time_direct, time_packed, time_general, BenchType.multi_input)
 
 def main():
     plot_tifs()
