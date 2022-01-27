2805510592cced3327a5b69e6a9d349fa381f79f
Kazuma Hirozawa <kazumasita@gmaili.com>
2022-01-27 17:25:54 +0900

    rename bench and result dirname

--- hlt_bench.sh
+++ hlt_bench.sh
@@ -9,7 +9,8 @@ poly_degree=2048
 poly_degree_multikernel=1024
 pack_num=1
 inputdim_multipoly=16
-err=hlt_result/err.log
+dirname_result_root=result
+err=$dirname_result_root/err.log
 
 # calculate input dimensions (n^2)
 for n in ${input_n[@]}
@@ -20,7 +21,7 @@ done
 
 # assume executable file is build/bin/direct_conv
 function directconv_multiinput(){
-    dirname=hlt_result/direct_conv/multiinput/k$kernel_dim
+    dirname=$dirname_result_root/direct_conv/multiinput/k$kernel_dim
     mkdir -p $dirname
     for input_dim in ${input_dims[@]}
     do
@@ -31,7 +32,7 @@ function directconv_multiinput(){
 }
 
 function directconv_multipoly(){
-    dirname=hlt_result/direct_conv/multipoly/k$kernel_dim
+    dirname=$dirname_result_root/direct_conv/multipoly/k$kernel_dim
     mkdir -p $dirname
     input_dim=$inputdim_multipoly
     for poly_degree in ${poly_degrees[@]}
@@ -43,30 +44,31 @@ function directconv_multipoly(){
 }
 
 function directconv_multikernel(){
-    mkdir -p hlt_result/direct_conv/multikernel
+    mkdir -p $dirname_result_root/direct_conv/multikernel
     input_dim=$inputdim_multipoly
     poly_degree=$poly_degree_multikernel
     for kernel_dim in ${kernel_dims[@]}
     do
         echo "Direct Convolution: (input, kernel, poly_degree) = ($input_dim, $kernel_dim, $poly_degree)"
-        output_path="hlt_result/direct_conv/multikernel/direct_conv$kernel_dim.txt"
+        output_path="$dirname/direct_conv$kernel_dim.txt"
         env build/bin/direct_conv $input_dim $kernel_dim $poly_degree 2>$err  > $output_path
     done
 }
 
 function packedconv_multiinput(){
-    mkdir -p hlt_result/packed_conv/multiinput
+    dirname=$dirname_result_root/packed_conv/multiinput
+    mkdir -p $dirname
     for input_dim in ${input_dims[@]}
     do
         echo "Packed Convolution: (input, kernel, pack_num) = ($input_dim, $kernel_dim, $pack_num)"
         echo "Polynomial Length: $poly_degree"
-        output_path="hlt_result/packed_conv/multiinput/packed_conv$input_dim.txt"
+        output_path="$dirname/packed_conv$input_dim.txt"
         env build/bin/packed_conv $input_dim $kernel_dim $poly_degree $pack_num 2>$err > $output_path
     done
 }
 
 function packedconv_multipoly(){
-    dirname=hlt_result/packed_conv/multipoly/k$kernel_dim
+    dirname=$dirname_result_root/packed_conv/multipoly/k$kernel_dim
     mkdir -p $dirname
     input_dim=$inputdim_multipoly
     for poly_degree in ${poly_degrees[@]}
@@ -78,19 +80,19 @@ function packedconv_multipoly(){
 }
 
 function packedconv_multikernel(){
-    mkdir -p hlt_result/packed_conv/multikernel
+    mkdir -p $dirname_result_root/packed_conv/multikernel
     input_dim=$inputdim_multipoly
     poly_degree=$poly_degree_multikernel
     for kernel_dim in ${kernel_dims[@]}
     do
         echo "Packed Convolution: (input, kernel, poly_degree, pack_num) = ($input_dim, $kernel_dim, $poly_degree,  $pack_num)"
-        output_path="hlt_result/packed_conv/multikernel/packed_conv$kernel_dim.txt"
+        output_path="$dirname_result_root/packed_conv/multikernel/packed_conv$kernel_dim.txt"
         env build/bin/packed_conv $input_dim $kernel_dim $poly_degree $pack_num 2>$err > $output_path
     done
 }
 
 function pc_toeplitz_multipoly(){
-    dirname=hlt_result/pc_toeplitz/multipoly/k$kernel_dim
+    dirname=$dirname_result_root/pc_toeplitz/multipoly/k$kernel_dim
     mkdir -p $dirname
     input_dim=$inputdim_multipoly
     for poly_degree in ${poly_degrees[@]}
@@ -102,30 +104,30 @@ function pc_toeplitz_multipoly(){
 }
 
 function general_lt_multiinput(){
-    mkdir -p hlt_result/general_lt/multiinput
+    mkdir -p $dirname_result_root/general_lt/multiinput
     # In general_lt, poly_degree 4096 or longer is too long to measure
     poly_degree=1024
     for input_dim in ${input_dims[@]}
     do
         echo "General Linear Transformation: ()"
-        output_path="hlt_result/general_lt/multiinput/general_lt$input_dim.txt"
+        output_path="$dirname_result_root/general_lt/multiinput/general_lt$input_dim.txt"
         env build/bin/general_lt $input_dim $kernel_dim $poly_degree 2>$err > $output_path
     done
 }
 
 function general_lt_multipoly(){
-    mkdir -p hlt_result/general_lt/multipoly
+    mkdir -p $dirname_result_root/general_lt/multipoly
     input_dim=$inputdim_multipoly
     for poly_degree in ${poly_degrees[@]}
     do
         echo "General Linear Transformation: (input, kernel, poly_degree) = ($input_dim, $kernel_dim, $poly_degree)"
-        output_path="hlt_result/general_lt/multipoly/general_lt$poly_degree.txt"
+        output_path="$dirname_result_root/general_lt/multipoly/general_lt$poly_degree.txt"
         env build/bin/general_lt $input_dim $kernel_dim $poly_degree 2>$err  > $output_path
     done
 }
 
 function clean(){
-    rm -rf hlt_result/*
+    rm -rf $dirname_result_root/*
 }
 
 #clean
@@ -136,4 +138,4 @@ pc_toeplitz_multipoly
 #packedconv_multiinput
 #directconv_multikernel
 #packedconv_multikernel
-general_lt_multipoly
+#general_lt_multipoly
